14012019809376b7d9e1e02313b3af7fd0ba298c
Luis Garcia <garciamluis93@gmail.com>
2024-09-22 02:48:32 -0700

    first commit

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 def hello_world():
     return "Hello from Luis Garcia!"
 
+def greet_person(name):
+    return f"Hello, {name}!"
+
 if __name__ == "__main__":
     print(hello_world())
+    print(greet_person("John"))
+
+
